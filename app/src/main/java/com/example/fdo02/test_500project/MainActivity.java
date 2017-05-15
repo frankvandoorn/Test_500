6dc2ce06d66500c47d4cf03fc3845319595e2697
frankvandoorn <f.vandoorn@saxion.nl>
2017-05-15 16:03:26 +0200

    Message

---
+++ app/src/main/java/com/example/fdo02/test_500project/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.fdo02.test_500project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //sausage
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
